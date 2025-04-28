96cabb08ebb49a1f3ff9c16ba48845dab4363018
Can Soylu <soylucan@hotmail.fr>
2025-04-28 17:59:50 +0300

    fix: correct python command for virtual environment creation

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ npm install &&
 npm run dev &
 
 cd backend && 
-pip3 -m venv venv && 
+python3 -m venv venv && 
 source venv/bin/activate && 
 pip3 install -r requirements.txt && 
 python3 manage.py runserver 0.0.0.0:8000 &
